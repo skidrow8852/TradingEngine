370846777c08a82e882d37d7e0cfd2d1fe0e33ca
skidrow8852 <amine12solo@gmail.com>
2024-11-25 09:38:36 +0100

    Added gitignore

---
+++ TradingEngine/TradingEngineServer.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using TradingEngineServer.Core;
+using TradingEngineServer.Core.Configuration;
+
+namespace TradingEngineServer.Core
+{
+    sealed class TradingEngineServer : BackgroundService, ITradingEngineServer
+    {   
+        private readonly ILogger _logger;
+        private readonly TradingServerConfig _config;
+        public TradingEngineServer(ILogger<TradingEngineServer> logger, IOptions<TradingServerConfig> config) {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _config = config.Value ?? throw new ArgumentNullException(nameof(config));
+            
+        }
+
+        public Task Run(CancellationToken token) => ExecuteAsync(token);
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation($"Started {nameof(TradingEngineServer)}");
+            while(!stoppingToken.IsCancellationRequested)
+            {
+
+            }
+            _logger.LogInformation($"Stopped {nameof(TradingEngineServer)}");
+            return Task.CompletedTask;
+        }
+    }
+}
