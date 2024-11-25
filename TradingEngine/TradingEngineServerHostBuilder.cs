ef1bd488deab8e0fe10598ce2ff06c43bdc151b7
skidrow8852 <amine12solo@gmail.com>
2024-11-25 09:33:17 +0100

    Init

---
+++ TradingEngine/TradingEngineServerHostBuilder.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using TradingEngineServer.Core.Configuration;
+namespace TradingEngineServer.Core
+{
+    public sealed class TradingEngineServerHostBuilder
+    {
+        public static IHost BuildTradingEngineServer() => Host.CreateDefaultBuilder()
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders(); 
+                    logging.AddConsole();    
+                 
+                })
+                .ConfigureServices((context, services) =>
+                {
+            // Start with config
+            services.AddOptions();
+            services.Configure<TradingServerConfig>(context.Configuration.GetSection(nameof(TradingServerConfig)));
+
+            // Add singleton objects
+            services.AddSingleton<ITradingEngineServer, TradingEngineServer>();
+
+            // Add Hosted service
+            services.AddHostedService<TradingEngineServer>();
+
+        }).Build();
+    }
+}
---
+++ TradingEngine/TradingServerConfig/TradingServerConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradingEngineServer.Core.Configuration
+{
+     class TradingServerConfig
+    {
+        public TradinEngineServerSettings TradinEngineServerSettings { get; set; }
+    }
+    class TradinEngineServerSettings
+    {
+        public int Port { get; set; }
+    }
+}
